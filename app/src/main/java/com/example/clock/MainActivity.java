df7b190637b6ac568ed2dba01ae13fc70823b327
Haechi <haechilim@icloud.com>
2021-03-27 10:45:44 +0900

    스톱워치 화면 구성

--- app/src/main/java/com/example/clock/MainActivity.java
+++ app/src/main/java/com/example/clock/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,19 +21,34 @@ public class MainActivity extends AppCompatActivity {
     AlarmAdepter alarmAdepter;
     Button editButton;
     Button completeButton;
+    Button addButton;
+    TextView title;
+    ListView alarmListView;
+    LinearLayout stopwatchPage;
+    TextView textAlarm;
+    TextView textStopwatch;
+    TextView textTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = findViewById(R.id.alarmList);
+        alarmListView = findViewById(R.id.alarmList);
+        stopwatchPage = findViewById(R.id.stopwatchPage);
         editButton = findViewById(R.id.editButton);
         completeButton = findViewById(R.id.completeButton);
+        addButton = findViewById(R.id.addButton);
+        title = findViewById(R.id.title);
+        textAlarm = findViewById(R.id.textAlarm);
+        textStopwatch = findViewById(R.id.textStopWatch);
+        textTimer = findViewById(R.id.textTimer);
 
         alarmAdepter = new AlarmAdepter(this);
 
-        listView.setAdapter(alarmAdepter);
+        alarmListView.setAdapter(alarmAdepter);
+
+        showPage(Constants.ALARM_PAGE);
 
         TimeThread timerThread = new TimeThread(this, alarmAdepter);
         timerThread.start();
@@ -50,12 +67,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.addButton).setOnClickListener(new View.OnClickListener() {
+        addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.putExtra(Constants.KEY_IS_NEW_ALARM, true);
                 startAddAlarmActivity(intent);
+            }
+        });
+
+        findViewById(R.id.alarmOfNavigationBar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPage(Constants.ALARM_PAGE);
+            }
+        });
+
+        findViewById(R.id.stopwatchOfNavigationBar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPage(Constants.STOPWATCH_PAGE);
+            }
+        });
+
+        findViewById(R.id.timerOfNavigationBar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPage(Constants.TIMER_PAGE);
             }
         });
     }
@@ -88,5 +126,40 @@ public class MainActivity extends AppCompatActivity {
         completeButton.setVisibility(isEditMode ? View.VISIBLE : View.GONE);
         alarmAdepter.editMode(isEditMode);
         alarmAdepter.notifyDataSetChanged();
+    }
+
+    private void showPage(int page) {
+        int white = Color.rgb(0xff, 0xff, 0xff);
+        int orange = Color.rgb(0xff, 0xa5, 0x00);
+
+        alarmListView.setVisibility(View.GONE);
+        stopwatchPage.setVisibility(View.GONE);
+
+        textAlarm.setTextColor(white);
+        textStopwatch.setTextColor(white);
+        textTimer.setTextColor(white);
+
+        if(page == Constants.ALARM_PAGE) {
+            title.setText("알람");
+            showActionBarButton(true);
+            alarmListView.setVisibility(View.VISIBLE);
+            textAlarm.setTextColor(orange);
+        }
+        else if(page == Constants.STOPWATCH_PAGE) {
+            title.setText("스톱워치");
+            showActionBarButton(false);
+            stopwatchPage.setVisibility(View.VISIBLE);
+            textStopwatch.setTextColor(orange);
+        }
+        else if(page == Constants.TIMER_PAGE) {
+            title.setText("타이머");
+            showActionBarButton(false);
+            textTimer.setTextColor(orange);
+        }
+    }
+
+    private void showActionBarButton(boolean visibility) {
+        editButton.setVisibility(visibility ? View.VISIBLE : View.INVISIBLE);
+        addButton.setVisibility(visibility ? View.VISIBLE : View.INVISIBLE);
     }
 }
--- app/src/main/java/com/example/clock/helper/Constants.java
+++ app/src/main/java/com/example/clock/helper/Constants.java
@@ -5,4 +5,7 @@ public class Constants {
     public final static String KEY_ALARM_TIME = "alarm time";
     public final static String KEY_LABEL = "label";
     public final static int RC_SUCCESS = 100;
+    public final static int ALARM_PAGE = 0;
+    public final static int STOPWATCH_PAGE = 1;
+    public final static int TIMER_PAGE = 2;
 }
